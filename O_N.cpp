3213edeaca323df7b9eba1e83dbc23ea9009d7ac
HridaySarder <hridoyaug12@gmail.com>
2024-06-10 11:48:13 +0600

    first commit

---
+++ O_N.cpp
@@ -0,0 +1,21 @@
+// O(N) big o notation
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        s += a[i];
+    }
+    cout << s << endl;
+    return 0;
+}
