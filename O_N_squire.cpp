73f213f7b999e274ed03bab18df53bef5e027925
HridaySarder <hridoyaug12@gmail.com>
2024-06-10 13:02:13 +0600

    added O_N_squire

---
+++ O_N_squire.cpp
@@ -0,0 +1,17 @@
+// O(sqr(N)) squire of N notation
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"Hello"<<endl;
+        }
+    }
+    return 0;
+}
